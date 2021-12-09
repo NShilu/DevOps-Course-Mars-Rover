781197bb273a91e7d31e992b76219c59fb3d15f5
Nirav Shilu <Nirav.Shilu@capita.com>
2021-12-09 15:26:26 +0000

    nasa repo to my git hub

--- app/app.py
+++ app/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os, requests, datetime
 
 
 app = Flask(__name__)
 
+API_KEY = os.getenv('API_KEY')
+
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
 
-@app.route('/mars')
+    response = requests.get("https://api.nasa.gov/planetary/apod?api_key={}".format(API_KEY))
+    r = response.json()
+    return render_template('index.html', landing_image=r['hdurl'])
+    
+
+@app.route('/mars', methods=["GET", "POST"])
 def mars():
-    return render_template('mars.html')
+
+    todaysDate = datetime.datetime.now().strftime('%Y-%m-%d')
+    filter = request.form.get('filter', todaysDate)
+
+    response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={}&camera=fhaz&api_key={}".format(filter, API_KEY))
+    r = response.json()
+    return render_template('mars.html', photos=r['photos'], todaysDate=todaysDate)
